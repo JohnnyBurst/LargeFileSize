aae5d5430bb86f926cef70000f235c1f4bab38a2
Johnny <johnjritenour@gmail.com>
2017-01-18 10:49:10 -0600

    Update script.ps1
    replaced _path with -path.

--- script.ps1
+++ script.ps1
@@ -7,7 +7,7 @@ $size = 100MB
 $limit = 100
 #press enter to accept the variable. Limits results returned to 100 entries.
 
-$largesizefiles = Get-ChildItem _path $path -recurse -ErrorAction "SilentlyContinue" | ? { $_.GetType().Name -eq "FileInfo" } | where-object {$_.length -gt $size} | sort-object -property length -descending | select-object @{Name="SizeInMB";Expression={$_.Length / 1MB}},@{Name="Path";Expression={$_.directory}} -first $limit
+$largesizefiles = Get-ChildItem -path $path -recurse -ErrorAction "SilentlyContinue" | ? { $_.GetType().Name -eq "FileInfo" } | where-object {$_.length -gt $size} | sort-object -property length -descending | select-object @{Name="SizeInMB";Expression={$_.Length / 1MB}},@{Name="Path";Expression={$_.directory}} -first $limit
 #run the command. Should take up to a minute to complete.
 
 $largesizefiles | Export-csv C:\largefilesizes.csv
